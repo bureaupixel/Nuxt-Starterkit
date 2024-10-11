059d3762957bd61569b50d07cbcb05794af862b0
Bureaupixel <bram.overstijns@gmail.com>
2024-10-11 13:36:16 +0200

    second commit

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
     <div>
         <nav class="mb-2">
         <ul>
-            <li class="pr-2 appTitle"><NuxtLink to="/home">Home</NuxtLink></li>
+            <li class="pr-2 appTitle"><NuxtLink to="/">Home</NuxtLink></li>
             <li class="pr-2 appTitle"><NuxtLink to="/example">Tailwind Example</NuxtLink></li>
         </ul>
         </nav>
